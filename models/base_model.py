3ecda1e791bb925a4b71721d2fbfcc1d7e0f0cae
Kamal <kamalmiftah01@gmail.com>
2024-03-04 17:03:10 +0100

    Fixing task 3

--- models/base_model.py
+++ models/base_model.py
@@ -9,8 +9,9 @@ class BaseModel():
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
-
+        print(self.created_at)
+        self.updated_at = self.created_at
+        print(self.updated_at)
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
@@ -23,5 +24,6 @@ class BaseModel():
         diction["__class__"] = self.__class__.__name__
         diction["created_at"] = self.created_at.isoformat()
         diction["updated_at"] = self.updated_at.isoformat()
+
         return diction
     
