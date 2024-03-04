3ae152253647f2ccf7d73c31bdbab2c00d37ff3b
Kamal <kamalmiftah01@gmail.com>
2024-03-04 21:00:09 +0100

    new

--- models/base_model.py
+++ models/base_model.py
@@ -6,12 +6,16 @@ import datetime
 
 class BaseModel():
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        print(self.created_at)
-        self.updated_at = self.created_at
-        print(self.updated_at)
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    self.key = value
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
