d592e049c54c8eb0ca566cde77711f6bfdb416b8
Joseph Eiba <josepheiba@icloud.com>
2024-03-05 23:56:32 +0100

    change import basemodel.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """ """
 import uuid
 import datetime
-from models.__init__ import storage
+import models
 
 class BaseModel():
 
@@ -18,14 +18,14 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = self.created_at
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         diction = dict(self.__dict__)
