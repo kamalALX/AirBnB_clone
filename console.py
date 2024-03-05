763d2ce3514cab3e1c2d12e4aee8079e792cff2f
Kamal <kamalmiftah01@gmail.com>
2024-03-05 14:56:23 +0100

    Task 6

---
+++ console.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" """
+import cmd
+import sys
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_EOF(self, line):
+        return True
+
+    def do_quit(self, arg):
+        'Quit command to exit the program'
+        return True
+
+    def do_emptyline(self, line):
+        'an empty line + ENTER shouldn’t execute anything'
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
