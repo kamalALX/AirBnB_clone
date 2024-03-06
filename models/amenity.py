a94d0aa1d43452c6be1252ae201dc70f8c40ea48
Joseph Eiba <josepheiba@icloud.com>
2024-03-07 00:10:24 +0100

    task9-fix-errors

--- models/amenity.py
+++ models/amenity.py
@@ -4,6 +4,6 @@ from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
+    name = ""
     def __init__(self, *args, **kwargs):
         super().__init__(self, *args, **kwargs)
-        self.name = ""
--- models/state.py
+++ models/state.py
@@ -4,6 +4,6 @@ from models.base_model import BaseModel
 
 
 class State(BaseModel):
+    name = ""
     def __init__(self, *args, **kwargs):
         super().__init__(self, *args, **kwargs)
-        self.name = ""
