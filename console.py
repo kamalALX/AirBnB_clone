f503374ae55f44e7cf8f3bcac5eaac797d687573
Joseph Eiba <josepheiba@icloud.com>
2024-03-06 16:32:29 +0100

    consoleupdate

--- console.py
+++ console.py
@@ -6,19 +6,27 @@ from models.user import User
 import models
 
 
+class_mapping = {
+    "BaseModel": BaseModel,
+    "User": User,
+}
+
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_create(self, line):
         'create BaseModel'
-        try:
-            create_instance = eval(line)()
-            create_instance.save()
-            print(create_instance.id)
-        except SyntaxError:
-            print("** class name missing **")
-        except NameError:
-            print("** class doesn't exist **")
+        if line in class_mapping:
+            try:
+                create_instance = class_mapping[line]()
+                create_instance.save()
+                print(create_instance.id)
+            except SyntaxError:
+                print("** class name missing **")
+            except NameError:
+                print("** class doesn't exist **")
+        else:
+            pass
 
     def do_show(self, line):
         'show BaseModel'
@@ -37,6 +45,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
 
     def do_destroy(self, line):
+        
         arg = line.split()
         length = len(arg)
 
@@ -53,22 +62,8 @@ class HBNBCommand(cmd.Cmd):
                 print("** no instance found **")
             models.storage.save()
 
-    def do_all(self, line):
-        dictio = models.storage.all()
-        lista_all = []
-        lista_class = []
-        for value in dictio.values():
-            lista_all.append(str(value))
-        if line:
-            if line in ["BaseModel", "User"]:
-                for item in lista_all:
-                    if line in item:
-                        lista_class.append(item)
-                print(lista_class)
-            else:
-                print("** class doesn't exist **")
-        else:
-            print(lista_all)
+    def do_all(self):
+        pass
 
     def do_update(self):
         pass
