78a8df894361e0c15ed9125da767d2e60846c855
Joseph Eiba <josepheiba@icloud.com>
2024-03-08 13:23:39 +0100

    pycodestype_models

--- models/city.py
+++ models/city.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-""" """
+"""
+class City
+"""
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
+    """
+    class City
+    """
     state_id = ""
     name = ""
 
