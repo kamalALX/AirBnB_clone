eb4855e7445ca638ce849cd8f624e6c6db7ca814
Joseph Eiba <josepheiba@icloud.com>
2024-03-08 13:15:21 +0100

    pycodestype_basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
-""" """
+"""
+BaseModel is the main model used for all classes
+"""
 import uuid
 import datetime
 import models
 
 
 class BaseModel():
+    """
+    BaseModel class
+    """
 
     def __init__(self, *_, **kwargs):
+        """
+        init method
+        """
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
                     if key == "created_at" or key == "updated_at":
-                        setattr(self, key, datetime.datetime.fromisoformat(value))
+                        iso_format_ = datetime.datetime.fromisoformat(value)
+                        setattr(self, key, iso_format_)
                     else:
                         setattr(self, key, value)
         else:
@@ -22,14 +31,26 @@ class BaseModel():
             models.storage.new(self)
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """
+        str return method
+        """
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__,
+            self.id,
+            self.__dict__)
 
     def save(self):
+        """
+        save method
+        """
         self.updated_at = datetime.datetime.now()
         self.__dict__.update({'updated_at': datetime.datetime.now()})
         models.storage.save()
 
     def to_dict(self):
+        """
+        to_dict method
+        """
         diction = dict(self.__dict__)
         diction["__class__"] = self.__class__.__name__
         diction["created_at"] = self.created_at.isoformat()
