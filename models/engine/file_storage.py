73d1639ff7e11f5e351ea0ea64bf5f5f4d626f5e
Kamal <kamalmiftah01@gmail.com>
2024-03-09 14:25:49 +0100

    ..

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-This module conatins FileStorage class
-"""
+""" this module conatins FileStorage class """
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -24,30 +22,22 @@ class_mapping = {
 
 
 class FileStorage():
-    """
-    This serializes instances to a JSON file
-    """
+    """ This serializes instances to a JSON file """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """
-        return all objects
-        """
+        """ return all objects """
         return FileStorage.__objects
 
     def new(self, obj):
-        """
-        create new object
-        """
+        """ create new object """
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             self.__objects[key] = obj
 
     def save(self):
-        """
-        save objects into file.json
-        """
+        """ save objects into file.json """
         formatted_dictionary = {}
         for key, obj in FileStorage.__objects.items():
             formatted_dictionary[key] = obj.to_dict()
@@ -55,9 +45,7 @@ class FileStorage():
             json.dump(formatted_dictionary, fileJSON)
 
     def reload(self):
-        """
-        loads from file.json into objects
-        """
+        """ loads from file.json into objects """
         try:
             new__objects = {}
             with open(FileStorage.__file_path, "r") as fileJSON:
