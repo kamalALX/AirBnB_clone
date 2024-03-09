d0177ab2dfdb8abaf937a3d37431ffeeea517dd1
Kamal <kamalmiftah01@gmail.com>
2024-03-09 14:50:15 +0100

    updating test_console

--- tests/test_console.py
+++ tests/test_console.py
@@ -26,6 +26,12 @@ class TestConsole(unittest.TestCase):
             HBNBCommand().onecmd("create")
             self.assertEqual(f.getvalue(), "** class name missing **\n")
 
+    def test_quit(self):
+        """test if quit works right"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("quit")
+            self.assertTrue(f.getvalue() == "")
+
 
 if __name__ == "__main__":
     unittest.main()
