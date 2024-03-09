a9237a35a69b2cb68dec85771aa6c8f9cc8c2e67
Joseph Eiba <josepheiba@icloud.com>
2024-03-09 19:44:42 +0100

    filestorage_update and test update

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -95,7 +95,6 @@ class TestFileStorage(unittest.TestCase):
 
     def test_reload_nonexistent_file(self):
         """Test reload method with a nonexistent file"""
-        os.remove(FileStorage._FileStorage__file_path)
         new_storage = FileStorage()
         new_storage.reload()
         all_objs = new_storage.all()
