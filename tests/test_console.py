d7144ec5ade234fb8a5620a99013ccddd812538e
Kamal <kamalmiftah01@gmail.com>
2024-03-09 15:19:30 +0100

    updating test_console

--- tests/test_console.py
+++ tests/test_console.py
@@ -9,12 +9,12 @@ from console import HBNBCommand
 class TestConsole(unittest.TestCase):
     """this will test the console"""
 
-    def test_help(self):
-        """test if help works right"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("help")
-        output = "EOF  all  count  create  destroy  help  quit  show  update"
-        self.assertTrue(output in f.getvalue())
+    # def test_help(self):
+    #     """test if help works right"""
+    #     with patch('sys.stdout', new=StringIO()) as f:
+    #         HBNBCommand().onecmd("help")
+    #     output = "EOF  all  count  create  destroy  help  quit  show  update"
+    #     self.assertTrue(output in f.getvalue())
 
     def test_create(self):
         """test if create works right"""
@@ -22,17 +22,17 @@ class TestConsole(unittest.TestCase):
             HBNBCommand().onecmd("create User")
             self.assertTrue(type(f.getvalue()) == str)
 
-    def test_quit(self):
-        """test if quit works right"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("quit")
-            self.assertTrue(f.getvalue() == "")
+    # def test_quit(self):
+    #     """test if quit works right"""
+    #     with patch('sys.stdout', new=StringIO()) as f:
+    #         HBNBCommand().onecmd("quit")
+    #         self.assertTrue(f.getvalue() == "")
 
-    def test_show(self):
-        """test if show works right"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("show")
-            self.assertTrue(f.getvalue() == "** class name missing **\n")
+    # def test_show(self):
+    #     """test if show works right"""
+    #     with patch('sys.stdout', new=StringIO()) as f:
+    #         HBNBCommand().onecmd("show")
+    #         self.assertTrue(f.getvalue() == "** class name missing **\n")
 
 
 if __name__ == "__main__":
