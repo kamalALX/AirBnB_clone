92b1af332baa49fff4ede9f0a47654846bd024d6
Kamal <kamalmiftah01@gmail.com>
2024-03-09 15:50:19 +0100

    updating test_file_storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,10 +5,6 @@ import os
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
 from models.engine.file_storage import FileStorage
 
 
