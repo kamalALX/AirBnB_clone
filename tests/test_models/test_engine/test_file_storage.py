ffab10c3041eb6910250343c6d423a4159748778
Kamal <kamalmiftah01@gmail.com>
2024-03-09 20:43:42 +0100

    updating test storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -87,10 +87,7 @@ class TestFileStorage(unittest.TestCase):
 
     def test_reload_nonexistent_file(self):
         """Test reload method with a nonexistent file"""
-        try:
-            os.remove(FileStorage._FileStorage__file_path)
-        except FileNotFoundError:
-            pass
+        os.remove(FileStorage._FileStorage__file_path)
         new_storage = FileStorage()
         new_storage.reload()
         all_objs = new_storage.all()
