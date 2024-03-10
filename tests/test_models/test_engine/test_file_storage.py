0bb672788d72c1a557c35edf3bd027b174cce052
Joseph Eiba <josepheiba@icloud.com>
2024-03-10 01:04:48 +0100

    file_s

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -85,6 +85,35 @@ class TestFileStorage(unittest.TestCase):
         all_objs = new_storage.all()
         self.assertIn(f"{obj1.__class__.__name__}.{obj1.id}", all_objs)
 
+    def test_reload_nonexistent_file(self):
+        """Test reload method with a nonexistent file"""
+        try:
+            os.remove(FileStorage._FileStorage__file_path)
+        except Exception:
+            pass
+
+        new_storage = FileStorage()
+        new_storage.reload()
+        all_objs = new_storage.all()
+        self.assertEqual(len(all_objs), 0)
+
+    def test_reload_empty_file(self):
+        """Test reload method with an empty file"""
+        open(FileStorage._FileStorage__file_path, "w").close()
+        new_storage = FileStorage()
+        new_storage.reload()
+        all_objs = new_storage.all()
+        self.assertEqual(len(all_objs), 0)
+
+    def test_reload_nonexistent_file2(self):
+        """Test reload method with a nonexistent file"""
+        self.storage.save()
+        os.remove(FileStorage._FileStorage__file_path)
+        new_storage = FileStorage()
+        new_storage.reload()
+        all_objs = new_storage.all()
+        self.assertEqual(len(all_objs), 0)
+
 
 if __name__ == "__main__":
     unittest.main()
