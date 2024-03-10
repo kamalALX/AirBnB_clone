cbee0c773047a4e152d05ee1aa88b5b1cd6068c0
Kamal <kamalmiftah01@gmail.com>
2024-03-10 02:22:08 +0100

    adding more test to test_console

--- tests/test_console.py
+++ tests/test_console.py
@@ -9,12 +9,11 @@ from console import HBNBCommand
 class TestConsole(unittest.TestCase):
     """this will test the console"""
 
-    def test_help(self):
-        """test if help works right"""
+    def test_create(self):
+        """test if create works right"""
         with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("help")
-        output = "EOF  all  count  create  destroy  help  quit  show  update"
-        self.assertTrue(output in f.getvalue())
+            HBNBCommand().onecmd("create")
+            self.assertEqual(f.getvalue(), "** class name missing **\n")
 
     def test_quit(self):
         """test if quit works right"""
